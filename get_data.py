3686768f928103a501abe44de08b9fed21e768f9
Trevor Winstral <trevor.winstral@gmail.com>
2020-06-05 13:33:30 +0200

    added relative path functionality as it was not writing csv file to correct location

--- get_data.py
+++ get_data.py
@@ -1,12 +1,15 @@
 import pandas as pd
+import os
 
 URL = 'https://raw.github.com/datasets/covid-19/master/data/countries-aggregated.csv'
-
+rel_path = '/'.join(os.path.abspath(__file__).split('/')[:-1])
+out_file = os.path.join(rel_path, 'data.csv')
+print(out_file)
 
 def main():
     try:
         data = pd.read_csv(URL, quotechar='"')
-        data.to_csv('data.csv')
+        data.to_csv(out_file)
         print('Data Retrieved\n')
     except Exception as e:
         print(f'URL: {URL}\n{e}\n\n')
